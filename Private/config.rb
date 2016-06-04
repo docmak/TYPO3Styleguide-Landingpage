829fd73468b25dba661b7d0d9edfacc0335fce41
Fabian Stein <stein@punkt.de>
2016-06-04 10:13:02 +0200

    TASK Base work add grunt files

---
+++ Private/config.rb
@@ -0,0 +1,21 @@
+# Syntax
+
+# Additional imports from sass in other projects
+add_import_path "node_modules/bootstrap-sass/assets/stylesheets"
+add_import_path "node_modules"
+
+preferred_syntax = :scss
+
+# Folder settings
+relative_assets = true					# because we're not working from the root
+css_dir = "../Public/Styles"			# where the CSS will saved
+sass_dir = "Styles"						# where our .scss files are
+images_dir = "../Public/Images"			# the folder with your images (relative path)
+fonts_dir = "../Public/Styles/Fonts"	# the folder with your fonts
+
+# preferred output style (can be overridden via the command line)
+output_style = :expanded 				# alternative: :compressed
+
+# environments
+environment = :development
+line_comments = true
