cf6495262f1f47d684b0c6d9e6cc9492182bd6c3
Fabian Stein <stein@punkt.de>
2016-06-15 17:25:11 +0200

    [ADD] Missing Data to last Commit

--- Private/javascript/test.js
+++ Private/javascript/test.js
@@ -1,3 +1,56 @@
 /**
  * Created by ry57 on 04.06.16.
  */
+
+// external js: isotope.pkgd.js
+jQuery(document).ready( function() {
+    // init Isotopevar
+    $grid = $('.grid').isotope({
+        itemSelector: '.task',
+        layoutMode: 'fitRows'
+    });
+    // filter functions
+    var filterFns = {
+        // show if number is greater than 50
+        //numberGreaterThan50: function() {
+        //    var number = $(this).find('.number').text();
+        //    return parseInt( number, 10 ) > 50;
+        //},
+        // show if name ends with -ium
+        categoryMarketing: function() {
+            var name = $(this).find('.category-marketing').get(0)
+            return name;
+        },
+        categoryDesign: function() {
+            var name = $(this).find('.category-design').get(0)
+            return name;
+        },
+        categoryText: function() {
+            var name = $(this).find('.category-text').get(0)
+            return name;
+        },
+        categoryTranslation: function() {
+            var name = $(this).find('.category-translation').get(0)
+            return name;
+        },
+        categoryDevelopment: function() {
+            var name = $(this).find('.category-development').get(0)
+            return name;
+        }
+    };
+    // bind filter button click
+    $('.filters-button-group').on( 'click', '.btn', function() {
+        var filterValue = $( this ).attr('data-filter');
+        // use filterFn if matches value
+        filterValue = filterFns[ filterValue ] || filterValue;
+        $grid.isotope({ filter: filterValue });
+    });
+    // change is-checked class on buttons
+    $('.btn-group').each( function( i, buttonGroup ) {
+        var $buttonGroup = $( buttonGroup );
+        $buttonGroup.on( 'click', '.btn', function() {
+            $buttonGroup.find('.is-checked').removeClass('is-checked');
+            $( this ).addClass('is-checked');
+        });
+    });
+})
