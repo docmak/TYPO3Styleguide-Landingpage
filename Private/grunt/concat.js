c27b02820e564f413168942a5944104d8236a46f
Fabian Stein <stein@punkt.de>
2016-06-16 14:03:21 +0200

    [TASK] Add Lightbox Style

--- Private/grunt/concat.js
+++ Private/grunt/concat.js
@@ -5,7 +5,9 @@ module.exports = {
             'node_modules/bootstrap-sass/assets/javascripts/bootstrap.js',
             'node_modules/handlebars/dist/handlebars.min.js',
             'node_modules/jquery-dateformat/dist/dateFormat.min.js',
-            'node_modules/isotope-layout/dist/isotope.pkgd.min.js'
+            'node_modules/isotope-layout/dist/isotope.pkgd.min.js',
+            'node_modules/featherlight/release/featherlight.min.js',
+
         ],
         dest: '../Public/JavaScript/libs.js'
     }
