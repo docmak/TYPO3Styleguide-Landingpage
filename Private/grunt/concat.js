0e70a97f5209af26929a3d074fd61c4259883221
Michel Löw <Michel@kleiner-Knecht.local>
2016-06-16 09:36:18 +0200

    [TASK] Update js includes

--- Private/grunt/concat.js
+++ Private/grunt/concat.js
@@ -4,7 +4,8 @@ module.exports = {
             'node_modules/jquery/dist/jquery.min.js',
             'node_modules/bootstrap-sass/assets/javascripts/bootstrap.js',
             'node_modules/handlebars/dist/handlebars.min.js',
-            'node_modules/jquery-dateformat/dist/dateFormat.min.js'
+            'node_modules/jquery-dateformat/dist/dateFormat.min.js',
+            'node_modules/isotope-layout/dist/isotope.pkgd.min.js'
         ],
         dest: '../Public/JavaScript/libs.js'
     }
